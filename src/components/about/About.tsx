b90a34085a430819e56cdc690fea0bdbddbc769a
alexdev0ver <salaris9315@gmail.com>
2020-12-29 17:13:49 +0700

    add sidebar, minor design changes

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import api from '../../integrations/api/ApiClient';
+import Sidebar from '../../utilities/sidebar/Sidebar';
 import Question from '../../types/Question';
+
+import './About.css';
 
 interface AboutState {
     questions: Question[]
@@ -35,28 +38,24 @@ export default class About extends Component<AboutProps, AboutState> {
         }
 
         return (
-            <div className="container-fluid overflow-hidden d-flex flex-column justify-content-center" style={{height:"100vh", background: "linear-gradient(120deg, #32E14C 50%, #fff 60%"}}>
-                <div className="d-none d-sm-flex flex-column justify-content-evenly"
-                     style={{backgroundColor:"#000000", opacity:"0.6", position:"absolute", top:"0", left:"0", width:"300px", height:"100vh"}}>
-                    <Link to="/">Back</Link>
-                    <Link to="/portfolio">Portfolio</Link>
-                    <Link to="/hobbies">Hobbies</Link>
-                </div>
-                <h1 className="text-center mb-4 mt-3">About Me</h1>
-                <div className="rounded overflow-auto container-fluid col-12 col-md-8 col-xl-5" style={{maxHeight:"70vh", backgroundColor:"#fff"}}>
+            <div className="container-fluid overflow-hidden d-flex flex-column justify-content-center aboutContainer">
+                <div className="vertical-line d-none d-sm-block"></div>
+                <h1 className="text-center mb-4 pt-5" style={{ borderTop:`${window.innerWidth > 680 ? "1px solid #fff" : "none"}`, marginLeft:"-15px"}}>About Me</h1>
+                <div className="rounded overflow-auto container-fluid col-12 col-md-8 col-xl-5 col-xxl-4" id="scrollbarStyle" style={{maxHeight:"70vh", backgroundColor:"#fff"}}>
                     {this.state.questions.map(el =>
                         <div className="mx-1 my-3 row" key={`${Math.random()*100}-questions`}>
-                            <h5 className="fst-italic text-md-end" style={{borderBottom:"1px solid #20c997"}}>{el.question}</h5>
+                            <h5 className="fst-italic fw-bolder text-md-end" style={{borderBottom:"1px solid #20c997", color: "#382929"}}>{el.question}</h5>
                             <div>{el.answer}</div>
                         </div>
                     )}
                     <div className="mx-1 my-3 row">
                         <h5 className="fst-italic text-md-end">Send you're question</h5>
-                        <textarea/>
+                        <textarea placeholder="How are you, Alex?"/>
                         <button>Send</button>
                     </div>
                 </div>
-                <Link to="/" style={{textDecoration:"none", color:"#2c3033"}} className="d-flex mt-4 justify-content-center">Back</Link>
+                <Sidebar/>
+                <Link to="/" className="d-md-none d-xs-flex mt-4 justify-content-center align-self-center" style={{textDecoration:"none", color:"#2c3033"}}>Back</Link>
             </div>
         )
     }
---
+++ src/utilities/sidebar/Sidebar.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import * as CSS from 'csstype';
+
+
+const Sidebar: React.FC = () => {
+
+    const [sidebarToggle, changeSidebarToggle] = useState(false);
+    const [hoverBack, hoveredBack] = useState(false);
+    const [hoverPortfolio, hoveredPortfolio] = useState(false);
+    const [hoverHobbies, hoveredHobbies] = useState(false);
+
+    const sidebarStyle : CSS.Properties = {
+        backgroundColor:"#000000",
+        opacity:"0.6",
+        position:"absolute",
+        top:"0",
+        left:"0",
+        width:`${sidebarToggle ? "200px" : "50px"}`,
+        height:"100vh",
+        transition:"0.6s"
+    }
+
+    const buttonStyle : CSS.Properties = {
+        backgroundColor:"#000",
+        opacity:"0.6",
+        border:"none",
+        outline:"none"
+    }
+
+    return (
+        <div id="sidebar" className="d-none d-sm-flex flex-column justify-content-evenly align-items-center"
+             style={sidebarStyle}>
+             <button
+                className="position-absolute top-0 left-0 w-100 btn btn-dark text-center"
+                style={buttonStyle}
+                onClick={(e) => {e.currentTarget.blur(); changeSidebarToggle(!sidebarToggle)}}>&#9776;</button>
+            <Link className={`${!sidebarToggle ? "d-none" : ""} w-100 text-center text-light fw-bolder`}
+                  to="/"
+                  style={{textDecoration:"none", borderBottom:`${hoverBack ? "2px solid #fff" : "none"}`, marginBottom:`${hoverBack ? "-2px" : "0"}`}}
+                  onMouseEnter={() => hoveredBack(true)}
+                  onMouseLeave={() => hoveredBack(false)}>Back</Link>
+            <Link className={`${!sidebarToggle ? "d-none" : ""} w-100 text-center text-light fw-bolder`}
+                  to="/portfolio"
+                  style={{textDecoration:"none", borderBottom:`${hoverPortfolio ? "2px solid #fff" : "none"}`, marginBottom:`${hoverPortfolio ? "-2px" : "0"}`}}
+                  onMouseEnter={() => hoveredPortfolio(true)}
+                  onMouseLeave={() => hoveredPortfolio(false)}>Portfolio</Link>
+            <Link className={`${!sidebarToggle ? "d-none" : ""} w-100 text-center text-light fw-bolder`}
+                  to="/hobbies"
+                  style={{textDecoration:"none", borderBottom:`${hoverHobbies ? "2px solid #fff" : "none"}`, marginBottom:`${hoverHobbies ? "-2px" : "0"}`}}
+                  onMouseEnter={() => hoveredHobbies(true)}
+                  onMouseLeave={() => hoveredHobbies(false)}>Hobbies</Link>
+        </div>
+    )
+}
+
+export default Sidebar;
